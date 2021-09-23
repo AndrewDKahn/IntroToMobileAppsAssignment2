3db62b0f663629125eea2e24ca630e599be3db93
AndrewDKahn <18akahn.stem@gmail.com>
2021-09-23 08:19:18 -0400

    Final touches

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -15,13 +15,13 @@ class FormActivity : AppCompatActivity() {
         val pass = (findViewById<Button>(R.id.editTextTextPassword)as EditText)
         val conPass = (findViewById<Button>(R.id.editTextTextPassword2)as EditText)
         findViewById<Button>(R.id.button).setOnClickListener {
-            if (name.text.isEmpty()) Toast.makeText(applicationContext, "Name is required.", Toast.LENGTH_SHORT)
-            if (emailAdd.text.isEmpty()) Toast.makeText(applicationContext, "Email is required.", Toast.LENGTH_SHORT)
-            if (pass.text.isEmpty()) Toast.makeText(applicationContext, "Password is required.", Toast.LENGTH_SHORT)
-            if (conPass.text.isEmpty()) Toast.makeText(applicationContext, "Password Confirmation is required.", Toast.LENGTH_SHORT)
-            if (pass.text.toString() != conPass.text.toString())Toast.makeText(applicationContext, "Passwords must match.", Toast.LENGTH_SHORT)
+            if (name.text.isEmpty()) Toast.makeText(applicationContext, "Name is required.", Toast.LENGTH_SHORT).show()
+            if (emailAdd.text.isEmpty()) Toast.makeText(applicationContext, "Email is required.", Toast.LENGTH_SHORT).show()
+            if (pass.text.isEmpty()) Toast.makeText(applicationContext, "Password is required.", Toast.LENGTH_SHORT).show()
+            if (conPass.text.isEmpty()) Toast.makeText(applicationContext, "Password Confirmation is required.", Toast.LENGTH_SHORT).show()
+            if (pass.text.toString() != conPass.text.toString())Toast.makeText(applicationContext, "Passwords must match.", Toast.LENGTH_SHORT).show()
             if (name.text.isNotEmpty() && emailAdd.text.isNotEmpty() && pass.text.isNotEmpty() && conPass.text.isNotEmpty()) {
-                Toast.makeText(applicationContext, "Welcome, ${name.text.toString()}!", Toast.LENGTH_SHORT)
+                Toast.makeText(applicationContext, "Welcome, ${name.text}!", Toast.LENGTH_SHORT).show()
             }
         }
     }
