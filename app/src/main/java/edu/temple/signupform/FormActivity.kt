78f4c5d1ad7e660cd3e9987c8c53b98904240908
AndrewDKahn <18akahn.stem@gmail.com>
2021-09-23 07:42:56 -0400

    Added the views to a layout, added empty onclicklistener

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,13 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener({});
     }
+
 }
